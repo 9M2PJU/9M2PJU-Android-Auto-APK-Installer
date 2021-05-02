c4fc54591dd3ea328fbbc022243e27375bbce3c7
Dmitry Rashupkin <annexhack@gmail.com>
2021-05-02 21:59:57 +0300

    Подправил patch для Screen2Auto

--- install.cmd
+++ install.cmd
@@ -1,14 +1,20 @@
 @echo off
 cls
-SET adb="..\adb\adb.exe"
-forfiles /P apk /S /M *.apk /c "cmd /c echo @path  & %adb% push @FILE /data/local/tmp/s2a.apk & %adb% shell pm install -i 'com.android.vending' -r /data/local/tmp/s2a.apk & %adb% shell rm /data/local/tmp/s2a.apk "
+
+SET adbv=adb\adb.exe
+forfiles /P apk /S /M *.apk /c "cmd /c echo @path  & ..\%adbv% push @FILE /data/local/tmp/@FILE & ..\%adbv% shell pm install -i 'com.android.vending' -r /data/local/tmp/@FILE & ..\%adbv% shell rm /data/local/tmp/@FILE"
 
 echo "inceptive.ru"
 
 choice /c YN /m "Patch screen2auto - Confirmation of MediaProjection?"
 goto run%errorlevel%
 :run1
-adb shell cmd appops set ru.inceptive.screentwoauto PROJECT_MEDIA allow
+%adbv% -d shell cmd appops set ru.inceptive.screentwoauto PROJECT_MEDIA allow
+%adbv% -d shell cmd appops set ru.inceptive.screentwoauto WRITE_SETTINGS allow
+%adbv% -d shell cmd appops set ru.inceptive.screentwoauto SYSTEM_ALERT_WINDOW allow
+%adbv% -d shell cmd appops set ru.inceptive.screentwoauto RUN_IN_BACKGROUND allow
+%adbv% -d shell cmd appops set ru.inceptive.screentwoauto WAKE_LOCK allow
+%adbv% shell am force-stop ru.inceptive.screentwoauto
 :run2
 echo Thanks a lot for using&pause&exit /b
 
